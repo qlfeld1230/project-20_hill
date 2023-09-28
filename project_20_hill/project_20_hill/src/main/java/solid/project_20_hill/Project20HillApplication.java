a4aa072429e46aa43dae65f92ae90ba5e265635d
박성호 <128385725+qlfeld1230@users.noreply.github.com>
2023-09-28 14:41:50 +0900

    Add files via upload

---
+++ project_20_hill/project_20_hill/src/main/java/solid/project_20_hill/Project20HillApplication.java
@@ -0,0 +1,13 @@
+package solid.project_20_hill;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Project20HillApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Project20HillApplication.class, args);
+	}
+
+}
---
+++ project_20_hill/project_20_hill/src/test/java/solid/project_20_hill/Project20HillApplicationTests.java
@@ -0,0 +1,13 @@
+package solid.project_20_hill;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Project20HillApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
