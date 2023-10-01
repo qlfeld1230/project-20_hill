33694224a8f79bf92e0a35c44df7f1241d13189a
SeongHo Park <qlfeld1324@gmail.com>
2023-10-01 21:28:10 +0900

    update

--- project_20_hill/project_20_hill/src/main/java/solid/project_20_hill/Project20HillApplication.java
+++ project_20_hill/project_20_hill/src/main/java/solid/project_20_hill/Project20HillApplication.java
@@ -2,10 +2,12 @@ package solid.project_20_hill;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+import javax.sql.DataSource;
+
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 public class Project20HillApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(Project20HillApplication.class, args);
 	}
